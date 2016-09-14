b6a51bae8da126c08507a14a85a282e013bd8b1d
gordonyli <gordonyli@gmail.com>
2016-09-14 08:59:30 -0400

    can insert into database, bootstrap, sidebar template - gordon

--- app.js
+++ app.js
@@ -2,16 +2,57 @@
  * Created by gordonli on 9/13/16.
  */
 var express = require("express");
-var app = express()
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http');
+var mysql = require('mysql');
 
 app.listen(3000, function() {
     console.log('Node server running @ http://localhost:3000');
-})
+});
 
 app.use('/node_modules',  express.static(__dirname + '/node_modules'));
-
-app.use('/style',  express.static(__dirname + '/style'));
+app.use('/css',  express.static(__dirname + '/css'));
+app.use('/lib',  express.static(__dirname + '/lib'));
+app.use('/js',  express.static(__dirname + '/js'));
+app.use('/views',  express.static(__dirname + '/views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',function(req,res){
-    res.sendFile('home.html',{'root': __dirname + '/public/views/'});
+    res.sendFile('home.html',{'root': __dirname + '/views'});
 })
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'mysqlpass',
+    database : 'Chariot'
+});
+
+connection.connect();
+
+connection.query('select * from users', function(err, rows) {
+    if (!err)
+        console.log('The solution is: ', rows);
+    else
+        console.log('Error while performing Query.');
+});
+
+
+app.post('/view1', function(req, res) {
+    console.log(req.body.desc);
+    console.log(req.body.title);
+    var post  = {username: req.body.desc, password: req.body.title};
+    connection.query('insert into users values (?,?)', [req.body.desc, req.body.title], function(err, rows) {
+        if (!err)
+            console.log('inserted into users');
+        else
+            console.log('Error while performing Query.');
+            console.log(err);
+    });
+    res.end();
+});
+
+connection.end();
+
---
+++ js/homeCtrl.js
@@ -0,0 +1,19 @@
+/**
+ * Created by rileyauten on 9/13/16.
+ */
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.test = "helloasfas";
+    $scope.sub = function() {
+        $http.post('/view1',$scope.formData).
+        success(function(data) {
+            console.log("posted successfully");
+        }).error(function(data) {
+            console.error("error in posting");
+        })
+    }
+
+}]);
+

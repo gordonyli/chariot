0888588b2b74f837901a50e321181cabe4784e21
gordonyli <gordonyli@gmail.com>
2016-09-21 10:30:04 -0400

    i hate passport

--- app/routes.js
+++ app/routes.js
@@ -7,17 +7,17 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.sendFile(path.resolve("views/index.html"));
+        res.sendFile(path.resolve("views/login.html"));
     });
 
     // =====================================
     // LOGIN ===============================
     // =====================================
     // show the login form
-    app.get('/login', function(req, res) {
+    app.get('/home', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.sendFile(path.resolve("views/login.html"));
+        res.sendFile(path.resolve("views/home.html"));
     });
 
     // process the login form
@@ -43,7 +43,7 @@ module.exports = function(app, passport) {
     // show the signup form
     app.get('/signup', function(req, res) {
         // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
+        res.render('signup.html', { message: req.flash('signupMessage') });
     });
 
     // process the signup form
@@ -59,7 +59,7 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
+        res.render('profile.html', {
             user : req.user // get the user out of session and pass to template
         });
     });
